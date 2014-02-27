ce40db0bb784c30f5ce15bc5e49a256898378188
Graham Holden <graham@opensussex.net>
2014-02-27 13:06:01 +0000

    add the basics for the time tracker commandline

---
+++ am.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    //"io/ioutil"
+    //"encoding/json"
+    "time"
+
+)
+
+
+func main() {
+    //fmt.Println(len(os.Args), os.Args)
+    
+    if(len(os.Args) > 1){
+    	arg_values := os.Args
+        switch(arg_values[1]){
+            case `help`,`h`:
+            
+            message := `
+                Welcome to am help
+                am is a command line time tracker
+                `;
+                fmt.Println(message)
+            break
+
+            case `time`,`t`:
+               fmt.Println(getTime())
+            break
+
+            default:
+                message := arg_values[1] + ` :argument not recognised try :am help for how to use`
+                fmt.Println(message)
+            break;
+        }
+
+    }else{
+        fmt.Println("No arguments provided try :am help for how to use")
+    }
+}
+
+func getTime() time.Time{
+    return  time.Now()
+}
